921a86d7e81b734116d1903d5e21b869ccdb4fac
Amazigh <amazigh.amrane@epita.fr>
2024-09-16 18:43:21 +0200

    active nav tab

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 // Function to highlight the current page in the navigation
+// Function to highlight the current page in the navigation
 function highlightCurrentPage() {
-    const currentPage = window.location.pathname.split('/').pop(); // Get the current page name
-    const navLinks = document.querySelectorAll('nav a');
+    let currentPage = window.location.pathname.split('/').pop(); // Get the current page name
 
+    // If the current page is empty (i.e., accessing the root domain), treat it as index.html
+    if (currentPage === '' || currentPage === 'index.html') {
+        currentPage = 'index.html'; 
+    }
+
+    const navLinks = document.querySelectorAll('nav a');
+    
     navLinks.forEach(link => {
         const linkHref = link.getAttribute('href');
-        if (linkHref === currentPage || (currentPage === 'index.html' && linkHref === 'index.html')) {
+        if (linkHref === currentPage) {
             link.classList.add('active');
         } else {
             link.classList.remove('active');
         }
     });
 }
+
 
 // Function to handle seminar date toggling
 function setupSeminarDateToggle() {
